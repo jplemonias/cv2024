91c697d8064a993369b15abfdc2a8e8d0fd6180f
jplemonias <psychoactif.gfwl@live.fr>
2024-11-25 15:46:29 +0100

    all is in the details

--- public/js/print.js
+++ public/js/print.js
@@ -1,20 +1,31 @@
 function ready() {
 
-    let msg = `Bonjour vous souhaitez imprimer mon CV\n`
-    msg += `Afin de profiter d'un meilleur confort de lecture en A4\n\n`
-    msg += `Dans les options d'impression, supprimez les marges.\n\n`
-    msg += `Merci pour l'intérêt porté.`
+    let msg = `Bonjour vous souhaitez imprimer mon CV\n`;
+    msg += `Afin de profiter d'un meilleur confort de lecture en A4\n\n`;
+    msg += `Dans les options d'impression, supprimez les marges.\n\n`;
+    msg += `Merci pour l'intérêt porté.`;
 
     const links = document.getElementById("job").querySelectorAll("a");
-
+    const cv_link = document.getElementById("cv_link");
+    const mail = document.getElementById("correspondence").lastChild;
+    const cv_link_ico = cv_link.querySelector("i");
+    const re = /\s:\s/g;
+    const cv_link_p = cv_link.getElementsByTagName("p");
+    const cv_link_p_node = cv_link.querySelector("p");
+    const cv_link_p_txt = cv_link_p[0].innerText;
+    const cv_link_p_txt_print = cv_link_p_txt.replace(re, "");
+    console.log(mail)
     window.onbeforeprint = function () {
         alert(msg);
         // if (count === 5) {
         //     addBreakPrintXP();
         // }
         for (let i = 0; i < links.length; i++) {
-            links[i].classList.add("invisible");
+            links[i].classList.add("displayNone");
         };
+        mail.classList.add("fontSizeForPrint");
+        cv_link_p_node.classList.add("fontSizeForPrint");
+        cv_link_p[0].innerHTML = cv_link_p_txt_print
     };
 
     window.onafterprint = function () {
@@ -22,8 +33,12 @@ function ready() {
         //     removeBreakPrintXP();
         // }
         for (let i = 0; i < links.length; i++) {
-            links[i].classList.remove("invisible");
+            links[i].classList.remove("displayNone");
         };
+        mail.classList.remove("fontSizeForPrint");
+        cv_link_p_node.classList.remove("fontSizeForPrint");
+        cv_link_p[0].innerHTML = cv_link_p_txt;
+        cv_link_p_node.prepend(cv_link_ico);
     };
 
 }
